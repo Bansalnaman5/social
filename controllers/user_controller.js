3cc9b62092843959fba702d059903dbc1b82573c
naman bansal <namanewq@gmail.com>
2020-12-11 15:35:23 +0530

    manual auth added successfully without passport

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,6 +1,23 @@
 const user=require('..//models/user');
 const { use } = require('../routes');
 
+module.exports.profile=function(req,res){
+    console.log(req.cookies);
+    if(req.cookies.user_id){
+        user.findById(req.cookies.user_id,function(err,us){
+            if (us){
+                return res.render('home');                
+            }
+            else{
+                return res.redirect('users/signin');
+            }
+        });
+    }
+    else{
+        return res.redirect('users/signin');
+    }
+};
+
 module.exports.signup=function(req,res){
     return res.render('user_signup',{
         title:"Social | Sign_Up"
@@ -36,5 +53,20 @@ module.exports.create=function(req,res){
 
 }
 module.exports.createSession=function(req,res){
-
+    user.findOne({email:req.body.email},function(err,us){
+        if(err){
+            console.log("error finding username!!");
+            return;
+        }
+        if(us){
+            if(us.password!=req.body.password){
+                return res.redirect('back');
+            }
+            res.cookie('user_id',us.id);
+            return res.redirect('/');
+        }
+        else{
+            return res.redirect('back');
+        }
+    })
 }
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,14 @@
 const express=require('express');
+const { route } = require('.');
 const router=express.Router();
 const userController=require('../controllers/user_controller');
-
 router.get('/signin',userController.signin);
 router.get('/signup',userController.signup);
+router.get('/profile',userController.profile);
+
 
 router.post('/create',userController.create);
-
+router.post('/create-session',userController.createSession);
 
 
 
