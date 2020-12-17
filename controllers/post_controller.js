3752e562c4c69adf4950585c9ff1e81de0f7cfb3
naman bansal <namanewq@gmail.com>
2020-12-17 17:17:01 +0530

    deleting comment added with auth

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const post=require('../models/post')
+const comments=require('../models/comments');
 
 module.exports.create_post=function(req,res){
     console.log(req.user.name);
@@ -11,5 +12,20 @@ module.exports.create_post=function(req,res){
             return;
         }
         return res.redirect('back');
+    });
+}
+
+module.exports.destroy=function(req,res){
+    post.findById(req.params.id,function(err,posts){
+        // .id means string of _id
+        if(posts.user==req.user.id){
+            posts.remove();
+            comments.deleteMany({post:req.params.id},function(err){
+                return res.redirect('back');
+            })
+        }
+        else{
+            res.redirect('back');
+        }
     });
 }
--- routes/post.js
+++ routes/post.js
@@ -4,5 +4,6 @@ const passport=require('passport');
 const posts=require('../controllers/post_controller');
 
 router.post('/create-post',passport.checkAuthentication,posts.create_post)
+router.get('/destroy/:id',passport.checkAuthentication,posts.destroy)
 
 module.exports=router;
