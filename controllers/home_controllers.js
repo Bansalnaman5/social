014243280603d91b87fbf7291312067c7a0db4db
naman bansal <namanewq@gmail.com>
2020-12-17 12:13:17 +0530

    post authentication and user with post  added

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,7 +1,61 @@
+const post=require('../models/post');
+const user=require('../models/user');
+
 module.exports.base=function(req,res){
-    return res.render('intro');
+    // post.find({},function(err,posts){
+    //     if(err){
+    //         console.log("error fetching posts!!Chal side ho");
+    //         return;
+    //     }
+    //     for(p of posts){
+    //         const id=p.user;
+    //         user.findById(id,function(err,username){
+    //             if(err){
+    //                 console.log("error in finding from user!!");
+    //                 return;
+    //             }
+    //             // console.log(username.name);
+    //             p.user=username.name;
+    //             console.log(p.user);
+    //         });
+    //     }
+        
+    //     return res.render('intro',{
+    //         posts:posts
+    //     });
+    // });
+
+    post.find({}).populate('user').exec(function(err,posts){
+        if(err){
+            console.log("error fetching posts!!Chal side ho");
+            return;
+        }
+        
+        return res.render('intro',{
+            posts:posts
+        });
+    });
+    
 };
 
 module.exports.prof=function(req,res){
-    return res.render('home');
+    // post.find({},function(err,posts){
+    //     if(err){
+    //         console.log("error fetching posts!!Chal side ho");
+    //         return;
+    //     }
+    //     return res.render('home',{
+    //         posts:posts
+    //     });
+    // });
+    post.find({}).populate('user').exec(function(err,posts){
+        if(err){
+            console.log("error fetching posts!!Chal side ho");
+            return;
+        }
+        
+        return res.render('intro',{
+            posts:posts
+        });
+    });
 }
--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,8 @@
 const express=require('express');
 const router=express.Router();
-// const passport=require('passport');
+const passport=require('passport');
 const posts=require('../controllers/post_controller');
 
-router.post('/create-post',posts.create_post)
+router.post('/create-post',passport.checkAuthentication,posts.create_post)
 
 module.exports=router;
